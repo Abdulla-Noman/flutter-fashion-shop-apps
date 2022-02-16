b66c33075b50b5e1d80177dd471adae098807b16
Noman <nomancomilla33@gmail.com>
2022-02-16 23:04:50 +0600

    Initial commit

---
+++ lib/utils/data.dart
@@ -0,0 +1,159 @@
+
+var profile = {
+  "name" : "Abdullah Al Noman",
+  "image" : "https://scontent.fcla1-1.fna.fbcdn.net/v/t39.30808-6/271684305_1278708269292540_2258143992445333613_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFzh9koI8hT_GhB1RdZkwqQrTF82aM_CR2tMXzZoz8JHZU7BSTAmxivmWO_BYU67OcbVyoWwaNVmJUFEYVG6dcM&_nc_ohc=JVJzE0FQQwwAX-sWcV_&_nc_ht=scontent.fcla1-1.fna&oh=00_AT8JpxTKmTISOmv30FrCZhAKpe9Norc6sT80d9CU4lQE0A&oe=62114F6A",
+  "email" : "abdulla14140@gmail.com"
+};
+
+List collections = [
+  {
+    'title': 'Men',
+    'image':
+        'https://cdn.shopify.com/s/files/1/0045/5104/9304/t/27/assets/AC_ECOM_SITE_2020_REFRESH_1_INDEX_M2_THUMBS-V2-1.jpg?v=8913815134086573859',
+  },
+  {
+    'title': 'Women',
+    'image':
+        'https://images.unsplash.com/photo-1509967419530-da38b4704bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8d29tYW4lMjBmYWNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80',
+  },
+  {
+    'title': 'Couple',
+    'image':
+        'http://sfwallpaper.com/images/couples-images-7.jpg',
+  },
+  {
+    'title': 'Kids',
+    'image': 'https://www.gamersdecide.com/sites/default/files/smilingkids.jpg',
+  },
+  {
+    'title': 'Others',
+    'image':
+        'https://www.thoughtco.com/thmb/C7RiS4QG5TXcBG2d_Sh9i4hFpg0=/3620x2036/smart/filters:no_upscale()/close-up-of-clothes-hanging-in-row-739240657-5a78b11f8e1b6e003715c0ec.jpg',
+  },
+];
+
+List populars = [
+  {
+    'name': 'Sleeve T-shirt',
+    'is_favorited' : false,
+    'price': '\$20.00',
+    'image': 'https://images.unsplash.com/photo-1561052623-76bca41705fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Summer T-shirt',
+    'is_favorited' : true,
+    'price': '\$10.00',
+    'image': 'https://images.unsplash.com/photo-1518609878373-06d740f60d8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Summer Version',
+    'is_favorited' : false,
+    'price': '\$25.00',
+    'image': 'https://media.istockphoto.com/photos/beautiful-fashionable-young-hipster-woman-in-sunglasses-with-long-picture-id1138814120?k=20&m=1138814120&s=612x612&w=0&h=5z7kHw_M7tUgoAqAH_5DQifKYXOSbbtZh-uOiLZHg4g=',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Loose Shirt',
+    'is_favorited' : true,
+    'price': '\$64.00',
+    'image': 'https://images.unsplash.com/photo-1548778943-5bbeeb1ba6c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=377&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Cotton T-shirt',
+    'is_favorited' : false,
+    'price': '\$33.00',
+    'image': 'https://images.unsplash.com/photo-1555085506-82b8d30d8380?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  }
+];
+
+List news = [
+  {
+    'name': 'Summer T-shirt',
+    'is_favorited' : false,
+    'price': '\$79.00',
+    'image': 'https://images.unsplash.com/photo-1516908205727-40afad9449a8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=389&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Slimmy Robe',
+    'is_favorited' : true,
+    'price': '\$65.00',
+    'image':'https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Summer Version',
+    'is_favorited' : false,
+    'price': '\$85.00',
+    'image': 'https://images.unsplash.com/photo-1524250502761-1ac6f2e30d43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Loose Shirt',
+    'is_favorited' : true,
+    'price': '\$30.00',
+    'image': 'https://images.unsplash.com/photo-1561052623-76bca41705fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Cotton T-shirt',
+    'is_favorited' : false,
+    'price': '\$34.00',
+    'image': 'https://images.unsplash.com/photo-1491349174775-aaafddd81942?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzF8fHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Summer Version',
+    'is_favorited' : false,
+    'price': '\$36.00',
+    'image': 'https://images.unsplash.com/photo-1498551172505-8ee7ad69f235?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Cotton T-shirt',
+    'is_favorited' : false,
+    'price': '\$19.00',
+    'image': 'https://images.unsplash.com/photo-1589156280159-27698a70f29e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTl8fHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Bat T-shirt',
+    'is_favorited' : true,
+    'price': '\$28.00',
+    'image': 'https://images.unsplash.com/photo-1542295669297-4d352b042bca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Njh8fHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Summer T-shirt',
+    'is_favorited' : true,
+    'price': '\$69.00',
+    'image': 'https://images.unsplash.com/photo-1509868918748-a554ad25f858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OTV8fHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+  {
+    'name': 'Sleeve T-shirt',
+    'is_favorited' : false,
+    'price': '\$66.00',
+    'image': 'https://images.unsplash.com/photo-1521567097888-2c5fc40a8660?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA1fHx3b21hbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
+    "description" : "Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.",
+    "sizes" : ["S", "M", "L", "XL"],
+  },
+];
